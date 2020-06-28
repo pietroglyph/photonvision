ef1ce860fee4119efe0ad6def00517420f6d9dbf
Banks Troutman <btrout.dhrs@gmail.com>
2020-06-28 14:44:44 -0400

    Did some stuff

--- photon-server/src/main/java/org/photonvision/vision/camera/USBCameraSource.java
+++ photon-server/src/main/java/org/photonvision/vision/camera/USBCameraSource.java
@@ -3,6 +3,7 @@ package org.photonvision.vision.camera;
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoMode;
+import edu.wpi.cscore.VideoSource;
 import edu.wpi.first.cameraserver.CameraServer;
 import java.util.*;
 import org.photonvision.common.configuration.CameraConfiguration;
@@ -15,7 +16,8 @@ public class USBCameraSource implements VisionSource {
     private final UsbCamera camera;
     private final USBCameraSettables usbCameraSettables;
     private final USBFrameProvider usbFrameProvider;
-    private final CameraConfiguration configuration;
+    public final CameraConfiguration configuration;
+    private final CvSink cvSink;
 
     private final QuirkyCamera cameraQuirks;
 
@@ -24,7 +26,9 @@ public class USBCameraSource implements VisionSource {
         this.camera = new UsbCamera(config.nickname, config.path);
         this.cameraQuirks =
                 new QuirkyCamera(camera.getInfo().productId, camera.getInfo().vendorId, config.baseName);
-        CvSink cvSink = CameraServer.getInstance().getVideo(this.camera);
+        cvSink = CameraServer.getInstance().getVideo(this.camera);
+        camera.setConnectionStrategy(VideoSource.ConnectionStrategy.kKeepOpen);
+        var err = cvSink.getError();
         this.usbCameraSettables = new USBCameraSettables(config);
         this.usbFrameProvider =
                 new USBFrameProvider(cvSink, usbCameraSettables.getFrameStaticProperties());
@@ -62,6 +66,7 @@ public class USBCameraSource implements VisionSource {
     public class USBCameraSettables extends VisionSourceSettables {
         protected USBCameraSettables(CameraConfiguration configuration) {
             super(configuration);
+            getAllVideoModes();
         }
 
         @Override
@@ -111,6 +116,7 @@ public class USBCameraSource implements VisionSource {
         public HashMap<Integer, VideoMode> getAllVideoModes() {
             if (videoModes == null) {
                 videoModes = new HashMap<>();
+                var rawModes = camera.enumerateVideoModes();
                 List<VideoMode> videoModesList = Arrays.asList(camera.enumerateVideoModes());
                 for (VideoMode videoMode : videoModesList) {
                     videoModes.put(videoModesList.indexOf(videoMode), videoMode);
